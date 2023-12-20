80e7ab4c757a43f53740eae3b7d6cb62c8f12fe8
Pawel Puszka <pawel.puszka@gmail.com>
2023-12-21 00:18:00 +0100

    exercise 2.4 done - reading and calculating total cost of shares in list of tuples

--- Work/report.py
+++ Work/report.py
@@ -1,4 +1,37 @@
 # report.py
 #
 # Exercise 2.4
+import csv
 
+
+def read_portfolio(filename):
+    portfolio = []
+    with open(filename, 'rt') as file:
+        file_rows = csv.reader(file)
+        header = next(file_rows)
+        for row in file_rows:
+            line = (row[0], int(row[1]), float(row[2]))
+            portfolio.append(line)
+
+    return portfolio
+
+
+def get_prices(portfl):
+    # share_cost = float()
+    total_cost = float()
+    for name, shares, price in portfl:
+        share_cost = shares * price
+        total_cost += share_cost
+
+    return total_cost
+
+
+# Exercise 2.5
+
+
+
+if __name__ == '__main__':
+    portfolio = read_portfolio('Data/portfolio.csv')
+    total_cost = get_prices(portfolio)
+
+    print(total_cost)
