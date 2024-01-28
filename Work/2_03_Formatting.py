4def91edb197a20b32fa9d3682a6159f2c107408
Pawel Puszka <pawel.puszka@gmail.com>
2024-01-28 22:25:03 +0100

    2.04 sequences done

--- Work/2_03_Formatting.py
+++ Work/2_03_Formatting.py
@@ -1,16 +1,7 @@
 import csv
 
 
-def print_portfolio_cost(file):
-    with open(file, 'rt') as file:
-        file_data = csv.reader(file)
-        header = next(file_data)
-        for line_nbr, line in enumerate(file_data, start=1):
-            try:
-                # print(int(line[1]))
-                print(f"{line[0]:>10s} {int(line[1]):>10} {float(line[2]):>10.2f} {(int(line[1])*float(line[2])):15.2f}")
-            except ValueError:
-                print(f"Line number: {line_nbr} Couldn't convert {line}")
+
 
 
 if __name__ == '__main__':
--- Work/2_04_Sequences.py
+++ Work/2_04_Sequences.py
@@ -32,21 +32,72 @@ def file_to_tuples(filename):
             print(line)
 
 
-def zip_function(filename):
+def zip_to_dict(filename):
+    list_of_pairs = list()
     with open(filename, 'rt') as file:
         file_text = csv.reader(file)
         header = next(file_text)
-        print(header)
         for line in file_text:
             print(line)
             # pair = zip(header, line)
-            print(dict(zip(header, line)))
+            list_of_pairs.append(dict(zip(header, line)))
+
+    for pair in list_of_pairs:
+        print(pair)
+
+
+def zip_to_list(filename):
+    x = list()
+    with open(filename, 'rt') as file:
+        file_text = csv.reader(file)
+        header = next(file_text)
+        for line in file_text:
+            x = list(zip(header, line))
+            print(x)
+
+
+
+# 2.15
+def print_portfolio_cost(file):
+    with open(file, 'rt') as file:
+        file_data = csv.reader(file)
+        header = next(file_data)
+        for line_nbr, line in enumerate(file_data, start=1):
+            try:
+                # print(int(line[1]))
+                print(f"{line[0]:>10s} {int(line[1]):>10} {float(line[2]):>10.2f} {(int(line[1])*float(line[2])):15.2f}")
+            except ValueError:
+                print(f"Line number: {line_nbr} Couldn't convert {line}")
+
+
+# 2.16
+def portfolio_cost(file):
+    total_cost = float()
+    with open(file, 'rt') as file:
+        file_data = csv.reader(file)
+        header = next(file_data)
+        for line_nbr, line in enumerate(file_data, start=1):
+            record = dict(zip(header, line))
+            try:
+                name = record['name']
+                shares = int(record['shares'])
+                price = float(record['price'])
+                total_cost += shares * price
+                # print(f"{name:>10s} {shares:>10} {price:>10.2f} {total_cost:15.2f}")
+            except ValueError:
+                print(f"Line number: {line_nbr} Couldn't convert {line}")
+    print(total_cost)
+
+
+# 2.17
 
 
 if __name__ == '__main__':
     # read_file('Data/portfolio.csv')
     # file_to_tuples('Data/portfolio.csv')
-    zip_function('Data/portfolio.csv')
+    # zip_to_dict('Data/portfolio.csv')
+    # zip_to_list('Data/portfolio.csv')
+    portfolio_cost('Data/portfolio.csv')
+    print()
+    portfolio_cost('Data/portfoliodate.csv')
 
-
-exercise 2.13
