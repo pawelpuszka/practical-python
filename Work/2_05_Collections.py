30d12ad7c423423226cbc043c286898e18c98580
Pawel Puszka <pawel.puszka@gmail.com>
2024-01-31 00:36:46 +0100

    Collections exercises done

--- Work/2_05_Collections.py
+++ Work/2_05_Collections.py
@@ -10,6 +10,7 @@ def portfolio(filename):
         header = next(lines)
 
         total_shares = Counter()
+        print(total_shares)
         for name, shares, price in lines:
             total_shares[name] += int(shares)
 
@@ -27,8 +28,43 @@ def portfolio_2(filename):
     print(holdings)
 
 
+def read_portfolio(filename):
+    portfolio = []
+    with open(filename, 'rt') as file:
+        file_rows = csv.reader(file)
+        header = next(file_rows)
+        for row in file_rows:
+            line = (row[0], int(row[1]), float(row[2]))
+            portfolio.append(line)
+
+    return portfolio
+
+
+def tabulate_portfolio(portfolio):
+    from collections import Counter
+    holdings = Counter()
+    for val in portfolio:
+        holdings[val[0]] += val[1]
+        # holdings[val[0]] += val[2]
+    # print(holdings)
+    return holdings
+
+
+def get_most_common(holdings, how_much):
+    from collections import Counter
+    print(holdings.most_common(how_much))
+
+
+def print_holdings(holdings):
+    print(holdings)
+
 
 if __name__ == '__main__':
-    portfolio_2('Data/portfolio.csv')
-
-# next Exercises
+    # portfolio('Data/portfolio.csv')
+    portfolio = read_portfolio('Data/portfolio.csv')
+    portfolio_2 = read_portfolio('Data/portfolio2.csv')
+    holdings = tabulate_portfolio(portfolio)
+    holdings2 = tabulate_portfolio(portfolio_2)
+    combined_holdings = holdings2 + holdings
+    print_holdings(combined_holdings)
+    get_most_common(combined_holdings, 2)
