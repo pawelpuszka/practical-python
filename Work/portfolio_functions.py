63988d1ef47636de6ff34e6c088cccb2b044c6ff
Pawel Puszka <pawel.puszka@gmail.com>
2024-02-14 23:22:28 +0100

    3 functions added to portfolio_functions file

---
+++ Work/portfolio_functions.py
@@ -0,0 +1,36 @@
+import csv
+
+
+def get_header(csv_file):
+    with open(csv_file, 'rt') as file:
+        data = csv.reader(file)
+        header = next(data)
+    return header
+
+
+def read_portfolio_dict(filename):
+    portfolio = []
+    with open(filename, 'rt') as file:
+        file_rows = csv.reader(file)
+        header = next(file_rows)
+        for row in file_rows:
+            record = {
+                      header[0]: row[0]
+                    , header[1]: int(row[1])
+                    , header[2]: float(row[2])
+            }
+            portfolio.append(record)
+
+    return portfolio
+
+
+def read_portfolio(filename):
+    portfolio = []
+    with open(filename, 'rt') as file:
+        file_rows = csv.reader(file)
+        header = next(file_rows)
+        for row in file_rows:
+            line = (row[0], int(row[1]), float(row[2]))
+            portfolio.append(line)
+
+    return portfolio
