fb58e6bdb680ba2c8b4d69b2d073b36ed437d25a
Pawel Puszka <pawel.puszka@gmail.com>
2024-05-07 23:44:06 +0200

    Exceptions checking exercises done

--- Work/3_03_Error_checking.py
+++ Work/3_03_Error_checking.py
@@ -7,56 +7,70 @@ def parse_csv(filename: str, delimiter: chr = ' ', selected_cols: list = None, t
     """
     if selected_cols and not has_header:
         raise RuntimeError("Selected columns should have headers")
+
     records = []
     with open(filename, 'rt') as file:
-        data = csv.reader(file, delimiter=delimiter)
-        if not has_header:
-            records = [tuple(record) for record in data if record is not None]
-            if types:
-                # if len(types) != len(records[0]):
-                    # raise RuntimeError('Wrong number of items in list')
-                for record in records:
-                    try:
-                        records = [tuple(func(item) for item, func in zip(record, types)) for record in records]
-                    except ValueError as e:
-                        print('Corrupted or missed value in the list')
-        else:
+        records = []
+        records_tmp = []
+
+        data = csv.reader(file)
+
+        if has_header:
             header = next(data)
-            records = [{head: item for head, item in zip(header, row)} for row in data if row is not None]
+
             if selected_cols:
-                records = [{item: record[item] for item in record if item in selected_cols} for record in records]
-                if types:
-                    # if len(types) != len(selected_cols):
-                        # raise RuntimeError('Wrong number of items in list')
-                    records = [{item: func(record[item]) for item, func in zip(record, types) if item in selected_cols} for record in records]
+                # records = [{item: record[item] for item in record if item in selected_cols} for record in data]
+                records = [{head: item for item, head in zip(record, header) if head in selected_cols} for record in data]
             else:
-                for row in data:
-                    if row is None:
-                        continue
-                    records.append(dict(zip(header, row)))
-                if types:
-                    # if len(types) != len(records[0]):
-                        # raise RuntimeError('Wrong number of items in list')
-                    records = [{item: func(record[item]) for item, func in zip(record, types) if item in header} for record in records]
+                records = [{head: item for item, head in zip(record, header)} for record in data]
+        else:
+            records = [tuple(record) for record in data]
+
+        if types:
+            if len(types) != len(records[0]):
+                raise RuntimeError('Wrong number of items in list with types')
+
+            if isinstance(records[0], tuple):  # checking only the first item of the list
+                for i, record in enumerate(records):
+                    try:
+                        records_tmp.append(tuple(func(item) for item, func in zip(record, types)))
+                    except ValueError as e:
+                        print("Problem with data conversion in line", i)
+            elif isinstance(records[0], dict):
+                for i, record in enumerate(records):
+                    try:
+                        records_tmp.append({item: func(record[item]) for item, func in zip(record, types)})
+                    except ValueError as e:
+                        print("Problem with data conversion in line", i+1)
+            records = records_tmp
 
     return records
 
 
 
 if __name__ == "__main__":
-    # print(parse_csv(filename=MISSING_DATA_CSV, types=[str, int, float]))
+    # print(parse_csv(filename=MISSING_DATA_CSV, selected_cols=['name', 'shares'], has_header=True, types=[str, int, float]))  # types=[str, int, float]))
+    print(parse_csv(filename=MISSING_DATA_CSV, has_header=True, types=[str, int, float]))
+    # print(parse_csv(filename=MISSING_DATA_CSV, has_header=False, types=[str, int, float]))
 
-    types = [str, int, float]
-    with open(MISSING_DATA_CSV, 'rt') as file:
-        data = csv.reader(file)
-        header = next(data)
-        records = [tuple(record) for record in data if record is not None]
-        records_tmp = []
-        for i, record in enumerate(records):
-            # records = [tuple(func(item) for item, func in zip(record, types)) for record in records]
-            try:
-                # records_tmp = [tuple(func(item) for item, func in zip(record, types))]
-                records_tmp = tuple(zip(record, types))
-            except ValueError as e:
-                print('Corrupted or missed value in the list on position ', i)
-        print(records_tmp)
+    # types = [str, int, float]
+    # with open(MISSING_DATA_CSV, 'rt') as file:
+    #     data = csv.reader(file)
+    #     header = next(data)
+    #     records = [tuple(record) for record in data if record is not None]
+    #     records_tmp = []
+    #     for i, record in enumerate(records):
+    #         print(header)
+    #         try:
+    #             record_tmp = [func(item) for item, func in zip(record, types)]
+    #             records_tmp.append(record_tmp)
+    #         except ValueError as e:
+    #             print("Wystąpił problem w linii", i)
+    #             print(e)
+    #         # records = [tuple(func(item) for item, func in zip(record, types)) for record in records]
+    #         # try:
+    #         #     # records_tmp = [tuple(func(item) for item, func in zip(record, types))]
+    #         #     records_tmp = tuple(zip(record, types))
+    #         # except ValueError as e:
+    #         #     print('Corrupted or missed value in the list on position ', i)
+    #     print(records_tmp)
