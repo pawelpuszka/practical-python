e516ae3bd96348613b726ae15189ccf8ab9bb04a
Pawel Puszka <pawel.puszka@gmail.com>
2024-05-14 15:47:15 +0200

    Newly refactored parse_csv function added to portfolio_functions module

--- Work/portfolio_functions.py
+++ Work/portfolio_functions.py
@@ -55,3 +55,53 @@ def compare_portfolio(filename_old, filename_new):
     r.print_total_gain(new_prices_pfolio)
     print()
     r.print_prices_diff(new_prices_pfolio)
+
+def parse_csv(filename: str, delimiter: chr = ' ', selected_cols: list = None, types: list = None, has_header: bool = False) -> list:
+    """
+    parse a csv file to list of dictionaries
+    """
+    if selected_cols and not has_header:
+        raise RuntimeError("Selected columns should have defined headers")
+
+    records = []
+    with open(filename, 'rt') as file:
+        records_tmp = []
+
+        data = csv.reader(file)
+
+        if has_header:
+            header = next(data)
+
+            if selected_cols:
+                # records = [{item: record[item] for item in record if item in selected_cols} for record in data]
+                records = [{head: item for item, head in zip(record, header) if head in selected_cols} for record in data]
+            else:
+                records = [{head: item for item, head in zip(record, header)} for record in data]
+        else:
+            records = [tuple(record) for record in data]
+
+        if types:
+            if len(types) != len(records[0]):
+                raise RuntimeError('Wrong number of items in list with types')
+
+            if isinstance(records[0], tuple):  # checking only the first item of the list
+                for i, record in enumerate(records):
+                    try:
+                        records_tmp.append(tuple(func(item) for item, func in zip(record, types)))
+                    except ValueError as e:
+                        print("Problem with data conversion in line", i)
+            elif isinstance(records[0], dict):
+                for i, record in enumerate(records):
+                    try:
+                        records_tmp.append({item: func(record[item]) for item, func in zip(record, types)})
+                    except ValueError as e:
+                        print("Problem with data conversion in line", i+1)
+            else:
+                raise RuntimeError('Unknown object type')
+
+            records = records_tmp
+
+    return records
+
+
+# przeczytać sekcję Commentary w 04_Modules
